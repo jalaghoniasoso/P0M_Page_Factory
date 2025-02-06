30d5789ab2fd17c994def016a3e5cf1b2d0b4f5d
jalaghoniasoso <jalaghoniasoso@gmail.com>
2025-02-06 11:07:20 +0400

    first commit

---
+++ src/main/java/ge/automation/BasePage.java
@@ -0,0 +1,58 @@
+package ge.automation;
+
+import ge.automation.pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+
+    }
+
+
+    public void enterText(By locator, String text) {
+        waitForElementToBeClickable(locator);
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    public void clickToElement(By locator){
+        driver.findElement(locator).click();
+    }
+
+    public void clickToElementWithWait(By locator){
+        waitForElementToBeClickable(locator);
+        driver.findElement(locator).click();
+    }
+
+    public void waitForElementToBeClickable(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public String getElementText(By locator){
+        waitForElementToBeVisible(locator);
+        return driver.findElement(locator).getText();
+    }
+
+    public void waitForElementToBeVisible(By locator){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public String getCssValue(By locator, String propertyName){
+        waitForElementToBeVisible(locator);
+        return driver.findElement(locator).getCssValue(propertyName);
+    }
+
+}
---
+++ src/test/java/ge/automation/tests/LoginTest.java
@@ -0,0 +1,32 @@
+package ge.automation.tests;
+
+import ge.automation.BaseTest;
+import ge.automation.pages.InventoryPage;
+import ge.automation.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+    @Test
+    public void testValidLogin() {
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("standard_user", "secret_sauce");
+
+        String expectedUrl = "https://www.saucedemo.com/inventory.html";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl, "Wrong Url");
+
+        InventoryPage inventoryPage = new InventoryPage(driver);
+        String expectedHeaderText = "Products";
+        String actualHeaderText = inventoryPage.getInventoryHeaderText();
+        Assert.assertEquals(actualHeaderText, expectedHeaderText);
+    }
+    @Test
+    public void testLoginButtonTextColor(){
+        LoginPage loginPage = new LoginPage(driver);
+
+        String expectedColor = "rgba(19, 35, 34, 1)";
+        String actualColor = loginPage.getLoginButtonTextColor();
+        Assert.assertEquals(actualColor, expectedColor);
+    }
+}
