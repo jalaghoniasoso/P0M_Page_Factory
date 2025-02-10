eb2456a4d978b1947cf25905a87a01bc601b40b4
jalaghoniasoso <jalaghoniasoso@gmail.com>
2025-02-10 11:51:46 +0400

    add

--- src/main/java/ge/automation/pages/LoginPage.java
+++ src/main/java/ge/automation/pages/LoginPage.java
@@ -35,7 +35,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-
 public class LoginPage extends BasePage {
     public LoginPage(WebDriver driver){
         super(driver);
---
+++ src/main/java/ge/automation/utils/TestListener.java
@@ -0,0 +1,38 @@
+package ge.automation.utils;
+
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result){
+        System.out.println("Test Started: " + result.getName());
+    }
+    @Override
+    public void onTestSuccess(ITestResult result){
+        System.out.println("Test Success: " + result.getName());
+    }
+    @Override
+    public void onTestFailure(ITestResult result){
+        System.out.println("Test Failure: " + result.getName());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result){
+        System.out.println("Test Failure: " + result.getName());
+    }
+
+    @Override
+    public void onStart(ITestContext context){
+        System.out.println("Test Suite Started: " + context.getName());
+    }
+
+    @Override
+    public void onFinish(ITestContext context){
+        System.out.println("Test Suite Started: " + context.getName());
+    }
+
+}
